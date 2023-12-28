aa39e25c92532cf943eca2030e4e00d97006f7d9
sovarim <abdul.taramov2001@gmail.com>
2023-12-29 01:13:43 +0300

    fonts

--- frontend/src/router/routerConfig.tsx
+++ frontend/src/router/routerConfig.tsx
@@ -3,12 +3,14 @@ import { HomePageAsync } from '../pages/HomePage/HomePage.async.tsx';
 import { MenuPageAsync } from '../pages/MenuPage/MenuPage.async.tsx';
 import { OrderPageAsync } from '../pages/OrderPage/OrderPage.async.tsx';
 import { CartPageAsync } from '../pages/CartPage/CartPage.async.tsx';
+import { MakeOrderPage } from '@/pages/MakeOrderPage/MakeOrderPage.tsx';
 
 export enum AppRoutes {
   HOME = 'home',
   MENU = 'menu',
   ORDER = 'order',
   CART = 'cart',
+  MAKE_ORDER = 'makeOrder',
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
@@ -16,6 +18,7 @@ export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MENU]: '/menu',
   [AppRoutes.ORDER]: '/orders',
   [AppRoutes.CART]: '/cart',
+  [AppRoutes.MAKE_ORDER]: 'make-order',
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -34,5 +37,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.CART]: {
     path: RoutePath.cart,
     element: <CartPageAsync />,
+  },
+  [AppRoutes.MAKE_ORDER]: {
+    path: RoutePath.makeOrder,
+    element: <MakeOrderPage />,
   },
 };

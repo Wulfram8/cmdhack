991f5e03b7b3b39cd4fee71cbcc69f715b343416
weirdmansmr <maga.saykhanov95@gmail.com>
2023-12-29 09:32:22 +0300

    now, cart will save on local storage

--- dto.ts
+++ dto.ts
@@ -1,49 +1,50 @@
 export type Client = {
-  id: number;
-  email: string;
-  fullName: string;
-  phone: string;
-  rewardPoints: number;
-  restarauntId?: number;
+	id: number;
+	email: string;
+	fullName: string;
+	phone: string;
+	rewardPoints: number;
+	restarauntId?: number;
 };
 
 export type Order = {
-  id: number;
-  client: Client;
-  products: {
-    quantity: number;
-    mealId: number;
-    notes: string[];
-  }[];
+	id: number;
+	client: Client;
+	products: {
+		quantity: number;
+		mealId: number;
+		notes: string[];
+	}[];
 };
 
 export type Ingredient = {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
 };
 
 export type Category = {
-  id: number;
-  name: string;
+	id: number;
+	name: string;
 };
 
 export type Meal = {
-  id: number;
-  name: string;
-  description: string;
-  ingredients: Ingredient[];
-  image: string;
-  price: number;
-  category: Category[];
+	id: number;
+	name: string;
+	description: string;
+	ingredients: Ingredient[];
+	image: string;
+	price: number;
+	category: Category[];
 };
 
 export type Restaurant = {
-  id: number;
-  name: string;
-  description: string;
-  categories: Category[];
-  image: string;
-  ordersCount: number;
-  meals: Meal[];
-  address: string;
+  categories: any;
+	id: number;
+	name: string;
+	description: string;
+	category: Category[];
+	image: string;
+	ordersCount: number;
+	meals: Meal[];
+	address: string;
 };
